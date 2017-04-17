d7e42ea006d76d3d4d0086750f9902b696344d44
Noel Miller <nmiller@avtex.com>
2017-04-17 14:15:13 -0500

    Created basic express server and built the app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 8080
+
+app.use(express.static(path.join(__dirname, 'angular-src/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'angular-src/dist/index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+})
