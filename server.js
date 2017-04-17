9dd859abbb595d6a63c42f549d724d90f5bc932d
Noel Miller <nmiller@avtex.com>
2017-04-17 17:18:18 -0500

    Added bower components due to the way express serves static pages

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'angular-src/dist/index.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'angular-src/dist/index.html'));
-});
-
 // Set listen port
 app.listen(port, () => {
   console.log('Server started on port ' + port);
